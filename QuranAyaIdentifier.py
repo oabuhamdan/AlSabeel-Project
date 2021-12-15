556de4b3f58761567f0e1340c60a2f36f37cc740
Osama <oabuhamdan@Osamas-MBP.lan>
2021-12-15 00:38:42 -0800

    Support Flask

--- QuranAyaIdentifier.py
+++ QuranAyaIdentifier.py
@@ -1,47 +1,57 @@
-import pandas as pd
-
-hash_table = dict()
-
-
-def update_hash_table(word, sub_table):
-    if word not in sub_table:
-        inner_sub_table = dict()
-        sub_table[word] = inner_sub_table
-        return inner_sub_table
-    else:
-        inner_sub_table = sub_table[word]
-        return inner_sub_table
+import json
+import os.path
 
 
-def load_file(file_path='quran-simple-clean.txt'):
-    with open(file_path, 'r') as f:
-        ayas = [line.strip() for line in f.readlines()]
-        return ayas
+class AyahIdentifier:
+    hash_table = dict()
 
+    def __init__(self):
+        if not os.path.isfile('output.txt'):
+            ayas = self.load_file()
+            self.process_file(ayas)
+            self.output()
+        else:
+            self.read_hashtable()
 
-def process_file(ayas):
-    for aya in ayas:
-        words = aya.split(' ')
-        for i, word in enumerate(words):  # الحمد لله رب العالمين
-            if i == 0:
-                inner_sub_table = update_hash_table(word, hash_table)
-            else:
-                inner_sub_table = update_hash_table(word, inner_sub_table)
+    def load_file(self, file_path='quran-simple-clean.txt'):
+        with open(file_path, 'r') as f:
+            ayas = [line.strip() for line in f.readlines()]
+            return ayas
 
+    def update_hash_table(self, word, sub_table):
+        if word not in sub_table:
+            inner_sub_table = dict()
+            sub_table[word] = inner_sub_table
+            return inner_sub_table
+        else:
+            inner_sub_table = sub_table[word]
+            return inner_sub_table
 
-def output():
-    with open('output.txt', 'w') as f:
-        f.write(str(hash_table))
+    def process_file(self, ayas):
+        for aya in ayas:
+            words = aya.split(' ')
+            for i, word in enumerate(words):
+                if i == 0:
+                    inner_sub_table = self.update_hash_table(word, self.hash_table)
+                else:
+                    inner_sub_table = self.update_hash_table(word, inner_sub_table)
 
+    def read_hashtable(self):
+        with open('output.txt', 'r') as f:
+            str = f.read().replace("'", '"')
+            self.hash_table = json.loads(str)
 
-def is_ayah(text):
-    words = text.strip().split()
-    sub_array = hash_table
-    for w in words:
-        if w in sub_array:
-            sub_array = sub_array[w]
-        if len(sub_array) == 0:
-            return True
+    def output(self):
+        with open('output.txt', 'w') as f:
+            f.write(str(self.hash_table))
 
-    return False
+    def is_ayah(self, text):
+        words = text.strip().split()
+        sub_array = self.hash_table
+        for w in words:
+            if w in sub_array:
+                sub_array = sub_array[w]
+            if len(sub_array) == 0:
+                return True
 
+        return False
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import time
-from QuranAyaIdentifier import *
 
-t1 = time.time()
-ayas = load_file()
-process_file(ayas)
-for ayah in ['إلا الذين آمنوا وعملوا الصالحات فلهم أجر غير ممنون', 'هل أتى على الإنسان حين من الدهر']:
-    print(f'Ayah "{ayah}" is {" an Ayah" if is_ayah(ayah) else "not an Ayah":}')
+from flask_cors import cross_origin, CORS
+from QuranAyaIdentifier import AyahIdentifier
 
-print(f"Took {time.time() - t1} to load the file, build the hashtable, and analyze the ayah(s)")
-output()
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/')
+def my_form():
+    return render_template('ayah_identifier.html')
+
+
+@app.route('/', methods=['POST'])
+@cross_origin()
+def my_form_post():
+    t1 = time.time()
+    ayah_Identifier = AyahIdentifier()
+    ayah = request.form['ayah']
+    res = f'"{ayah}" is {" an Ayah" if ayah_Identifier.is_ayah(ayah) else "not an Ayah":}'
+    dur = f"Took {time.time() - t1} to load the file, build the hashtable, and analyze the ayah(s)"
+
+    return render_template('ayah_identifier.html', data={'res': res, 'dur': dur})
+
+
+if __name__ == '__main__':
+    app.run()
